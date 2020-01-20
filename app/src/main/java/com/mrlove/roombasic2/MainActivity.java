032338b69bd7745ab7d7b8622cc798b4697d0f93
Mrlove133481 <1334819077@qq.com>
2020-01-20 09:05:57 +0800

    完成两个布局的切换，并实现点击时间，并且进一步精简了databinding的数据绑定逻辑

--- app/src/main/java/com/mrlove/roombasic2/MainActivity.java
+++ app/src/main/java/com/mrlove/roombasic2/MainActivity.java
@@ -2,6 +2,7 @@ package com.mrlove.roombasic2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -16,38 +17,53 @@ import com.mrlove.roombasic2.domain.Word;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-     MyViewModel myViewModel;
+    MyViewModel myViewModel;
     //DatabindingBinding 由框架编译时生成，负责通知界面同步更新(命名方式：xml文件名 + Binding)；
-     ActivityMainBinding binding;
-     MyAdapter myAdapter;
+    ActivityMainBinding binding;
+    MyAdapter myAdaptercard, myAdapternormal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //DataBindingUtil 将布局文件与Activity关联,生成DatabindingBinding实例binding；
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         //获取ViewModel实例
-        myViewModel = new ViewModelProvider(this,new SavedStateViewModelFactory(getApplication(),this)).get(MyViewModel.class);
+        myViewModel = new ViewModelProvider(this, new SavedStateViewModelFactory(getApplication(), this)).get(MyViewModel.class);
         //为布局文件设置源数据
         binding.setData(myViewModel);
-        myAdapter = new MyAdapter();
+        myAdapternormal = new MyAdapter(false);
+        myAdaptercard = new MyAdapter(true);
         binding.recyclerview.setLayoutManager(new LinearLayoutManager(this));
-        binding.recyclerview.setAdapter(myAdapter);
+        binding.recyclerview.setAdapter(myAdapternormal);
+
+        binding.switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    binding.recyclerview.setAdapter(myAdaptercard);
+                } else {
+                    binding.recyclerview.setAdapter(myAdapternormal);
+                }
+            }
+        });
 
         //为实现LiveData的数据设置观察者，以便当数据改变时通知UI更新数据
         myViewModel.getAllWordsLive().observe(this, new Observer<List<Word>>() {
             @Override
             public void onChanged(List<Word> words) {
-                  myAdapter.setAllWords(words);
-                  myAdapter.notifyDataSetChanged();
+                myAdaptercard.setAllWords(words);
+                myAdaptercard.notifyDataSetChanged();
+                myAdapternormal.setAllWords(words);
+                myAdapternormal.notifyDataSetChanged();
             }
         });
         //添加
         binding.buttoninsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Word word1 = new Word("hello","你好");
-                Word word2 = new Word("world","世界");
-                myViewModel.insertWords(word1,word2);
+                Word word1 = new Word("hello", "你好");
+                Word word2 = new Word("world", "世界");
+                myViewModel.insertWords(word1, word2);
 
             }
         });
@@ -63,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         binding.buttonupdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Word word = new Word("haha","哈哈");
+                Word word = new Word("haha", "哈哈");
                 word.setId(40);
                 myViewModel.updateWords(word);
             }
@@ -72,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         binding.buttondelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Word word = new Word("haha","哈哈");
+                Word word = new Word("haha", "哈哈");
                 word.setId(40);
                 myViewModel.deleteWords(word);
             }
--- app/src/main/java/com/mrlove/roombasic2/MyAdapter.java
+++ app/src/main/java/com/mrlove/roombasic2/MyAdapter.java
@@ -1,12 +1,17 @@
 package com.mrlove.roombasic2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.mrlove.roombasic2.databinding.CellCardBinding;
 import com.mrlove.roombasic2.databinding.CellNormalBinding;
 import com.mrlove.roombasic2.domain.Word;
 
@@ -16,12 +21,19 @@ import java.util.List;
 //RecyclerView内容管理器类
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
-    private static final String TAG = "mrLove" ;
     private List<Word> allWords = new ArrayList<>();
+    private boolean useCardView;
+    private CellNormalBinding normalBinding;
+    private CellCardBinding cardBinding;
 
     //返回所有的数据
     void setAllWords(List<Word> allWords) {
         this.allWords = allWords;
+    }
+
+    //初始化布局标志位
+    public MyAdapter(boolean useCardView) {
+        this.useCardView = useCardView;
     }
 
     //当适配器创建的时候调用
@@ -29,21 +41,35 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //RecyclerView中调用Databinding，绑定布局，获取binding实例
-        CellNormalBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.cell_normal, parent, false);
-        //把binding作为参数，返回一个自定义的MyViewHolder
-        return new MyViewHolder(binding);
+        //根据标识位初始化获取不同的binding
+        if (useCardView) {
+            cardBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.cell_card, parent, false);
+            //把binding作为参数，返回一个自定义的MyViewHolder
+            return new MyViewHolder(cardBinding);
+        } else {
+            normalBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.cell_normal, parent, false);
+            return new MyViewHolder(normalBinding);
+        }
     }
-    //当调用ViewHolder是响应
+
+    //当调用ViewHolder时响应
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
         //获取当前位置的一行数据
         Word word = allWords.get(position);
-        //获取一个binding
-        CellNormalBinding binding = holder.getBinding();
         //设置数据
-        binding.textNumber.setText(String.valueOf(position + 1)); //让页面显示从1开始，而不用显示word.getId数据库中实际的位置
-        binding.textEnglish.setText(word.getWord());
-        binding.textChinese.setText(word.getChineseMeaning());
+        holder.textNumber.setText(String.valueOf(position + 1)); //让页面显示从1开始，而不用显示word.getId数据库中实际的位置
+        holder.textEnglish.setText(word.getWord());
+        holder.textChinese.setText(word.getChineseMeaning());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://m.youdao.com/dict?le=eng&q=" + holder.textEnglish.getText());
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(uri);
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -51,19 +77,27 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         //返回一个数据的容量
         return allWords.size();
     }
+
     //把onCreateViewHolder方法中的binding存起来，可以下次再用。这样做的好处就是不必每次都到布局文件中去拿到你的View，提高了效率。
     static class MyViewHolder extends RecyclerView.ViewHolder {
-        private CellNormalBinding binding;
-
+        private CellNormalBinding normalBinding;
+        private CellCardBinding cardBinding;
+        private TextView textNumber, textEnglish, textChinese;
+        //两种参数不同的构造函数初始化不同的布局控件
         MyViewHolder(CellNormalBinding binding) {
             super(binding.getRoot());
-            this.binding = binding;
-
+            normalBinding = binding;
+            textNumber = normalBinding.textNumber;
+            textEnglish = normalBinding.textEnglish;
+            textChinese = normalBinding.textChinese;
         }
 
-        CellNormalBinding getBinding() {
-            return binding;
+        MyViewHolder(CellCardBinding binding) {
+            super(binding.getRoot());
+            cardBinding = binding;
+            textNumber = cardBinding.textNumber;
+            textEnglish = cardBinding.textEnglish;
+            textChinese = cardBinding.textChinese;
         }
-
     }
 }
